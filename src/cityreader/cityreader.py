df7535ea3b2d9338a8a5a01741839bc04cbcadb1
benjamin-solt <bensolt@gmail.com>
2020-05-08 11:47:11 -0400

    cities file is hard

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -2,10 +2,13 @@
 # fields for name, lat and lon (representing latitude and longitude).
 
 class City:
-  def __init__(self, name, lat, lon)
-  self.name = name
-  self.lat = lat
-  self.lon = lon
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f"{self.name}: ({self.lat}, {self.lon})"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -21,10 +24,18 @@ class City:
 # should not be loaded into a City object.
 cities = []
 
+import csv
+
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+  with open("src/cityreader/cities.csv") as cityfile:
+    cityreader = csv.reader(cityfile)
+    for i, row in enumerate(cityreader):
+      if i:
+        cities.append(City(row[0], float(row[3]), float(row[4])))
     
     return cities
 
